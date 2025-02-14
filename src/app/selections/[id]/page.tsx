cfb107981dc96d0b259e168d0df9836d62cb73f9
cawauchi6204 <cawauchi6204@gmail.com>
2025-02-15 05:59:54 +0900

    ✨ リンクを写真に追加

--- src/app/selections/[id]/page.tsx
+++ src/app/selections/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { getSelectionItems, getSelection } from "../../actions";
 import Image from "next/image";
 import { Metadata } from "next";
+import Link from "next/link";
 
 export async function generateMetadata({
   params,
@@ -51,17 +52,21 @@ export default async function SelectionPage({
             key={item.package_id}
             className="bg-gray-800 rounded-xl overflow-hidden shadow-xl hover:shadow-2xl transition-shadow duration-300"
           >
-            <div className="aspect-w-16 aspect-h-9 relative">
-              <Image
-                src={item.image_url}
+            <Link href={item.fanza_url} className="aspect-w-16 aspect-h-9 relative">
+              <div className="aspect-w-16 aspect-h-9 relative">
+                <Image
+                  src={item.image_url}
                 alt={item.title}
                 className="object-cover w-full h-full hover:opacity-90 transition-opacity duration-300"
                 width={1000}
-                height={1000}
-              />
-            </div>
+                  height={1000}
+                />
+              </div>
+            </Link>
             <div className="p-6 space-y-4">
-              <h2 className="text-xl font-bold text-white mb-3">{item.package_title}</h2>
+              <h2 className="text-xl font-bold text-white mb-3">
+                {item.package_title}
+              </h2>
               <p className="text-gray-300 text-sm leading-relaxed">
                 {item.description}
               </p>
