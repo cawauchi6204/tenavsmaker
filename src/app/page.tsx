a0b6cc06ae4fbc7225a80d12d0aac284675a8bb3
cawauchi6204 <cawauchi6204@gmail.com>
2025-02-28 23:16:29 +0900

    ✨ 修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
-// import AVSelector from "@/components/av-selector";
-// import { getRecentSelections } from "@/app/actions";
-// import dayjs from "dayjs";
-import DummyPage from "@/components/dummy-page";
+import AVSelector from "@/components/av-selector";
+import { getRecentSelections } from "@/app/actions";
+import dayjs from "dayjs";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -33,16 +32,15 @@ export const metadata: Metadata = {
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
-  // const recentSelections = await getRecentSelections();
+  const recentSelections = await getRecentSelections();
 
   // revalidate を 0 に設定して動的レンダリングを強制
 
   // Date オブジェクトを dayjs を使って ISO 文字列に変換
-  // const serializedSelections = recentSelections.map((selection) => ({
-  //   ...selection,
-  //   created_at: dayjs(selection.created_at).format(),
-  // }));
+  const serializedSelections = recentSelections.map((selection) => ({
+    ...selection,
+    created_at: dayjs(selection.created_at).format(),
+  }));
 
-  // return <AVSelector initialRecentSelections={serializedSelections} />;
-  return <DummyPage />;
+  return <AVSelector initialRecentSelections={serializedSelections} />;
 }
